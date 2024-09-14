c5fdd9f2cd682689fa541101fd645a6e31b24d38
shahzad980311 <118654090+shahzad980311@users.noreply.github.com>
2024-09-14 13:58:23 +0500

    this is index.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<header class="header">
+    <div class="header_body">
+        <a href="index.php" class="logo">TechoBling</a>
+        <nav class="navbar">
+            <a href="">Add Products</a>
+            <a href="view_products.php">View Products</a>
+            <a href="">Shopit</a>
+        </nav>
+          <!-- shopping cart -->
+          <a href="" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup>4</sup></span></a>
+    </div>
+  </header>
